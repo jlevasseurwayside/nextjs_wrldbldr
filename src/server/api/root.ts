3928e7fa1bd4675df47721716b3d67d902a1fa08
James Levasseur <jameslevasseur1@gmail.com>
2024-01-09 12:21:12 -0500

    switched from post to settlement

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,4 @@
-import { postRouter } from "~/server/api/routers/post";
+import { settlementRouter } from "~/server/api/routers/settlement";
 import { createTRPCRouter } from "~/server/api/trpc";
 
 /**
@@ -7,7 +7,7 @@ import { createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
+  settlement: settlementRouter,
 });
 
 // export type definition of API
